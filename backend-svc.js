0e7a41968fc326e26e52e656fa7499f6b5a28284
itsayushs <ayushstarksharma@gmail.com>
2021-10-11 01:29:22 +0530

    added zipkin exporter.

--- backend-svc.js
+++ backend-svc.js
@@ -1,3 +1,20 @@
+const { NodeTracerProvider } = require('@opentelemetry/node')
+const { ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/tracing')
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin')
+const provider = new NodeTracerProvider()
+const consoleExporter = new ConsoleSpanExporter()
+const spanProcessor = new SimpleSpanProcessor(consoleExporter)
+provider.addSpanProcessor(spanProcessor)
+provider.register()
+
+const zipkinExporter = new ZipkinExporter({
+  url: 'http://localhost:9411/api/v2/spans',
+  serviceName: 'backend-service'
+})
+
+const zipkinProcessor = new SimpleSpanProcessor(zipkinExporter)
+provider.addSpanProcessor(zipkinProcessor)
+
 const express = require('express');
 const app = express();
 
--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const { NodeTracerProvider } = require('@opentelemetry/node')
+const { ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/tracing')
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin')
+const provider = new NodeTracerProvider()
+const consoleExporter = new ConsoleSpanExporter()
+const spanProcessor = new SimpleSpanProcessor(consoleExporter)
+provider.addSpanProcessor(spanProcessor)
+
+const zipkinExporter = new ZipkinExporter({
+  url: 'http://localhost:9411/api/v2/spans',
+  serviceName: 'frontend-service'
+})
+
+const zipkinProcessor = new SimpleSpanProcessor(zipkinExporter)
+provider.addSpanProcessor(zipkinProcessor)
+provider.register()
+
+
 const axios = require('axios');
 const express = require('express')
 const app = express()
@@ -12,9 +30,9 @@ async function getAnimeData() {
     }
 }
 
-app.get('/dashboard', async (req, res) => {
+app.get('/', async (req, res) => {
     const movies = await getAnimeData()
     res.send(JSON.stringify({ dashboard: movies }))
   })
 
-app.listen(8080, () => { console.log(`Listening at http://localhost:8080`)})
+app.listen(8080, () => { console.log(`Frontend is running on http://localhost:8080`)})
